490ab30ade6ce2ec429cc7b9d585d944f53a643e
admin <tjuqxb@hotmail.com>
2020-11-02 08:46:46 -0800

    'change'

---
+++ src/main/java/qxb/SpringExercise/SpringWebApp/domain/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package qxb.SpringExercise.SpringWebApp.domain.repositories;
+import org.springframework.data.repository.CrudRepository;
+
+import qxb.SpringExercise.SpringWebApp.domain.Author;
+
+public class AuthorRepository extends CrudRepository<Author,Long> {
+
+}
---
+++ src/main/java/qxb/SpringExercise/SpringWebApp/domain/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package qxb.SpringExercise.SpringWebApp.domain.repositories;
+import org.springframework.data.repository.CrudRepository;
+import qxb.SpringExercise.SpringWebApp.domain.Book;
+
+public class BookRepository extends CrudRepository<Book,Long> {
+
+}
